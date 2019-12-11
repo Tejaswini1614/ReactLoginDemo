61024833f248937171cd6c029ea62cfbdf4d5f27
Tejaswini1614 <tejaswinikoli1295@gmail.com>
2019-12-11 17:46:40 +0530

    added login service

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
 import '../assets/common.css';
+import Service from '../services/Service';
 export default class Login extends Component {
     constructor(props) {
         super(props)
+        this.state=({
+         userName:"",
+         password:"",
+
+        })
         this.serviceObj = new Service();
     }
     
     handleSubmit=()=>{
-        this.serviceObj.addNewRecord(this.state).then(
+        this.serviceObj.login(this.state).then(
             response => {
 
             });
@@ -18,10 +24,10 @@ export default class Login extends Component {
             <div className="container">
             <div className="login-panel">
           
-            <label for="psw"><b>Email Id:</b></label>
-            <input type="text" placeholder="Enter Name" name="psw" required/><br/>
+            <label for="psw"><b>Username:</b></label>
+            <input type="text" onChange={(e)=>{this.setState({userName:e.currentTarget.value})}}  placeholder="Enter Username" name="psw" required/><br/>
             <label for="psw-repeat"><b>Password:</b></label>
-            <input type="text" placeholder="Enter Address" name="psw-repeat" required/><br/>
+            <input type="text" onChange={(e)=>{this.setState({password:e.currentTarget.value})}} placeholder="Enter Password" name="psw-repeat" required/><br/>
 
 
     <button type="submit" onClick={this.handleSubmit} class="registerbtn">Login</button>
--- src/services/Service.js
+++ src/services/Service.js
@@ -17,4 +17,21 @@ export default class Service {
         window.location.href = "/invalidUrl";
       });
   };
+
+
+  login = encrypted_data => {
+    return fetch(`${config.baseUrl}/login`, {
+      method: "POST",
+      body: encrypted_data,
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => res.text())
+      .then(res => {
+        return res;
+      }).catch(error => {
+        window.location.href = "/invalidUrl";
+      });
+  };
 }
